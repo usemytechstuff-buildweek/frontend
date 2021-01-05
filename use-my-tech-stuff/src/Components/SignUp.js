ca0418796c97ef1d910e8c7c48a7b8e71fd082a2
Samuel Lee <samuelxlee@gmail.com>
2021-01-05 13:57:38 -0800

    added link to external api, made login and signup post properly to said api

--- use-my-tech-stuff/src/Components/SignUp.js
+++ use-my-tech-stuff/src/Components/SignUp.js
@@ -3,15 +3,16 @@ import { useHistory } from 'react-router-dom';
 import axios from 'axios';
 import * as yup from 'yup';
 import SignUpValidation from '../Validation/SignupValidation';
+import { BACKEND_LINK } from "../constants";
 
-const initialFormValues ={
+const initialFormValues = {
     username: '',
     password: '',
     firstName: '',
     lastName: '',
 }
 
-const initialFormErrors ={
+const initialFormErrors = {
     username: '',
     password: '',
     firstName: '',
@@ -20,41 +21,41 @@ const initialFormErrors ={
 
 const initialDisabled = true;
 
-const SignUp = () =>{
+const SignUp = () => {
     const [signUpForm, setSignUpForm] = useState(initialFormValues);
     const [signUpUser, setSignUpUser] = useState([]);
     const [signUpFormErrors, setSignUpFormErrors] = useState(initialFormErrors);
     const [signUpDisabled, setSignUpDisabled] = useState(initialDisabled);
     const history = useHistory();
 
-    const registerNewUser = newUser =>{
-        axios.post('/api/auth/register', newUser)
-        .then((res) =>{
-            setSignUpUser([...signUpUser, res.data]);
-            setSignUpForm(initialFormValues);
-        })
-        .catch((err) =>{
-            console.log(err, 'error');
-        })
+    const registerNewUser = newUser => {
+        axios.post(`${BACKEND_LINK}/api/auth/register`, newUser)
+            .then((res) => {
+                setSignUpUser([...signUpUser, res.data]);
+                setSignUpForm(initialFormValues);
+            })
+            .catch((err) => {
+                console.log(err, 'error');
+            })
 
     }
 
 
-    const changeSignUp = (name, value) =>{
+    const changeSignUp = (name, value) => {
         yup.reach(SignUpValidation, name)
-        .validate(value)
-        .then(() =>{
-            setSignUpFormErrors({
-                ...signUpFormErrors,
-                [name]: '',
-            });
-        })
-        .catch((err) =>{
-            setSignUpFormErrors({
-                ...setSignUpFormErrors,
-                [name]: err.errors[0]
+            .validate(value)
+            .then(() => {
+                setSignUpFormErrors({
+                    ...signUpFormErrors,
+                    [name]: '',
+                });
             })
-        })
+            .catch((err) => {
+                setSignUpFormErrors({
+                    ...setSignUpFormErrors,
+                    [name]: err.errors[0]
+                })
+            })
         setSignUpForm({
             ...signUpForm,
             [name]: value
@@ -63,38 +64,38 @@ const SignUp = () =>{
 
 
 
-    const signUpFormSubmit = () =>{
-        const newUser ={
-            username:signUpForm.username.trim(),
-            password:signUpForm.password.trim(),
-            firstName:signUpForm.firstName.trim(),
-            lastName:signUpForm.lastName.trim(),
+    const signUpFormSubmit = () => {
+        const newUser = {
+            username: signUpForm.username.trim(),
+            password: signUpForm.password.trim(),
+            firstName: signUpForm.firstName.trim(),
+            lastName: signUpForm.lastName.trim(),
         }
         registerNewUser(newUser);
     };
 
 
-    useEffect(() =>{
-        SignUpValidation.isValid(signUpForm).then((valid) =>{
+    useEffect(() => {
+        SignUpValidation.isValid(signUpForm).then((valid) => {
             setSignUpDisabled(!valid);
         })
-    },[signUpForm])
+    }, [signUpForm])
 
 
 
-    const onSubmit = evt =>{
+    const onSubmit = evt => {
         evt.preventDefault();
         signUpFormSubmit();
     };
 
-    const onChange = evt =>{
-        const {name, value} = evt.target;
+    const onChange = evt => {
+        const { name, value } = evt.target;
         changeSignUp(name, value)
     }
 
 
 
-    return(
+    return (
         <form onSubmit={onSubmit}>
             <div className='signup-page'>
                 <div className='signup-form'>
@@ -111,41 +112,41 @@ const SignUp = () =>{
 
                         <label>First Name:
                             <input
-                            type='text'
-                            name='firstName'
-                            placeholder='First Name'
-                            value={signUpForm.firstName}
-                            onChange={onChange}   
+                                type='text'
+                                name='firstName'
+                                placeholder='First Name'
+                                value={signUpForm.firstName}
+                                onChange={onChange}
                             />
                         </label>
 
                         <label>Last Name:
                             <input
-                            type='text'
-                            name='lastName'
-                            placeholder='Last Name'
-                            value={signUpForm.lastName}
-                            onChange={onChange}   
+                                type='text'
+                                name='lastName'
+                                placeholder='Last Name'
+                                value={signUpForm.lastName}
+                                onChange={onChange}
                             />
                         </label>
 
                         <label>Username:
                             <input
-                            type='text'
-                            name='username'
-                            placeholder='Username'
-                            value={signUpForm.username}
-                            onChange={onChange}   
+                                type='text'
+                                name='username'
+                                placeholder='Username'
+                                value={signUpForm.username}
+                                onChange={onChange}
                             />
                         </label>
 
                         <label>Password:
                             <input
-                            type='text'
-                            name='password'
-                            placeholder='Password'
-                            value={signUpForm.password}
-                            onChange={onChange}   
+                                type='text'
+                                name='password'
+                                placeholder='Password'
+                                value={signUpForm.password}
+                                onChange={onChange}
                             />
                         </label>
 
@@ -155,7 +156,7 @@ const SignUp = () =>{
 
                 </div>
             </div>
-        
+
         </form>
     )
 
---
+++ use-my-tech-stuff/src/constants.js
@@ -0,0 +1,1 @@
+export const BACKEND_LINK = "https://use-my-tech-stuff-bw.herokuapp.com";
