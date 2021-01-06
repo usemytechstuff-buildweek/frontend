f40bff747a296da61c9e14737282c064c770f61c
Diego Roman <diegormnv209@gmail.com>
2021-01-06 15:35:43 -0800

    Component to dislpay items for rent

---
+++ use-my-tech-stuff/src/Components/TechForRent.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+
+
+
+const TechForRent = () =>{
+
+
+    return(
+        <div>
+        if(userType === 'owner'? <Owner/>)
+        <Renter></Renter>
+        </div>
+    )
+}
+
+export default TechForRent;
