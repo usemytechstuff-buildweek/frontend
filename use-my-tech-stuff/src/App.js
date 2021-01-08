4e6f343bc9a6099dbccbfe73596545c007eb3cf6
Diego Roman <diegormnv209@gmail.com>
2021-01-07 16:20:26 -0800

    made basic layout of landing pages

--- use-my-tech-stuff/src/App.js
+++ use-my-tech-stuff/src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import NavBar from './Components/Navigation';
 import SignUp from './Components/SignUp';
 import Login from './Components/Login';
+import Listings from './Components/LandingPage/Listings';
+import Owner from './Components/LandingPage/Owner';
 import OwnerItems from "./Components/OwnerItems";
 import UpdateItem from "./Components/UpdateItem";
 import PrivateRoute from "./Components/PrivateRoute";
@@ -17,6 +19,8 @@ function App() {
         <Switch>
           <Route exact path='/signup' component={SignUp} />
           <Route exact path='/' component={Login} />
+          <Route exact path='/listings' component={Listings}/>
+          <Route exact path='/owner' component={Owner}/>
           <PrivateRoute path="/tech-protected/:id/update" component={UpdateItem} />
           <PrivateRoute path='/tech-protected' component={TechForRent} />
         </Switch>
